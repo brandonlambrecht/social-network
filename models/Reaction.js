6ec135819972fc861e16eb95a953a8b341dce54a
brandonlambrecht <brandonjlambrecht@gmail.com>
2024-01-05 12:57:49 -0600

    added route, controller, api folder

--- models/Reaction.js
+++ models/Reaction.js
@@ -20,7 +20,7 @@ const reactionSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      // Use a getter method to format the timestamp on query
+      
       get: createdAtVal => new Date(createdAtVal).toLocaleString()
     }
   });
